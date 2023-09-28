5c73ea73c10e836b5a917bc3b87a66a1d6c65cf5
jarenass001 <146364990+jarenass001@users.noreply.github.com>
2023-09-28 17:37:55 +0200

    Initial commit

---
+++ src/main/java/es/unex/mdai/reservasFablab/ReservasFablabApplication.java
@@ -0,0 +1,13 @@
+package es.unex.mdai.reservasFablab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasFablabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasFablabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/unex/mdai/reservasFablab/ReservasFablabApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unex.mdai.reservasFablab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasFablabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
