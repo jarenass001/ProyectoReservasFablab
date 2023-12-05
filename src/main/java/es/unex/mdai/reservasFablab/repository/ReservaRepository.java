838d9088dc61714bfe69dd1bbc4fbbae601799d7
isampepr <146364410+isampepr@users.noreply.github.com>
2023-12-05 12:48:48 +0100

    Añadido a ReservaReopsitory de findByMaquina

--- src/main/java/es/unex/mdai/reservasFablab/repository/ReservaRepository.java
+++ src/main/java/es/unex/mdai/reservasFablab/repository/ReservaRepository.java
@@ -1,12 +1,12 @@
 package es.unex.mdai.reservasFablab.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
+import es.unex.mdai.reservasFablab.model.Maquina;
 import es.unex.mdai.reservasFablab.model.Reserva;
 import es.unex.mdai.reservasFablab.model.Usuario;
 
 public interface ReservaRepository extends CrudRepository<Reserva, Long> {
 	Iterable<Reserva> findByUsuario(Usuario usuario);
+	Iterable<Reserva> findByMaquina(Maquina maquina);
 }
