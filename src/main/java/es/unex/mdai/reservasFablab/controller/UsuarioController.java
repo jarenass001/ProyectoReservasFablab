938fd1d89f7012dfc4e1bbebfac04faf01daf912
isampepr <146364410+isampepr@users.noreply.github.com>
2023-12-07 18:33:05 +0100

    Añadido las vistas de home para user y admin

--- src/main/java/es/unex/mdai/reservasFablab/controller/UsuarioController.java
+++ src/main/java/es/unex/mdai/reservasFablab/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,17 +36,42 @@ public class UsuarioController {
 		}
 	}
 
+	@GetMapping("/admin/menuPrincipal")
+	public String showAdminMenu(Model model, Usuario usuario, Boolean admin) {
+		usuarioService.crearUsuario(new Usuario("A", "1234"));
+		usuario = usuarioService.findAllUsers().iterator().next();
+		admin = true;
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("admin", admin);
+		return "menuAdmin"; // devuelve la vista a renderizar
+	}
+	@GetMapping("/user/menuPrincipal")
+	public String showUserMenu(Model model, Usuario usuario, Boolean admin) {
+		usuarioService.crearUsuario(new Usuario("B", "1234"));
+		usuario = usuarioService.findAllUsers().iterator().next();
+		admin = false;
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("admin", admin);
+		return "menuUser"; // devuelve la vista a renderizar
+	}
 	@GetMapping("/admin/ListarUsuarios")
 	public String showUsersTable(Model model) {
 		List<Usuario> usuarioList = (List<Usuario>) usuarioService.findAllUsers();
 		model.addAttribute("usuarios", usuarioList);
 		return "ListarUsuarios"; // devuelve la vista a renderizar
 	}
-
+	@GetMapping("/user/VerUsuario/{id}/{admin}")
+	// @PathVariable: El parámetro forma parte de la URL
+	public String showMyUserData(@PathVariable("id") Long usuarioId, Model model,@PathVariable("admin") Boolean admin) {
+		model.addAttribute("usuario", usuarioService.findUsuarioById(usuarioId).get());
+		model.addAttribute("admin", admin);
+		return "VerUsuario";
+	}
 	@GetMapping("/user/VerUsuario/{id}")
 	// @PathVariable: El parámetro forma parte de la URL
-	public String showUpdateUsuarioForm(@PathVariable("id") Long usuarioId, Model model) {
+	public String showUpdateUsuarioForm(@PathVariable("id") Long usuarioId, Model model, Boolean admin) {
 		model.addAttribute("usuario", usuarioService.findUsuarioById(usuarioId).get());
+		model.addAttribute("admin", admin);
 		return "VerUsuario";
 	}
 
@@ -57,7 +81,6 @@ public class UsuarioController {
     }
 	@PostMapping("/admin/addUsuario")
 	public String addUsuario(Usuario usuario, Model model) {
-		System.out.println("\t UsuarioController::addUsuario");
 		model.addAttribute("usuarios", usuarioService.crearUsuario(usuario));
 		return "redirect:/admin/ListarUsuarios";
 	}
