b6a593e672172f94e9200ead25f9ea5f6768ea12
Juan Luis Arenas <146364990+jarenass001@users.noreply.github.com>
2023-12-11 18:30:51 +0100

    crud parcial finalizado

--- src/main/java/es/unex/mdai/reservasFablab/controller/MaquinaController.java
+++ src/main/java/es/unex/mdai/reservasFablab/controller/MaquinaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import es.unex.mdai.reservasFablab.model.Maquina;
 import es.unex.mdai.reservasFablab.service.MaquinaService;
@@ -25,6 +27,24 @@ public class MaquinaController {
 	public String verListaMaquinas(Model model) {
 		model.addAttribute("listaDeMaquinas", (List<Maquina>) maquinaService.findAllMaquinas());
 		return "listarMaquinas";
+	}
+	
+	@PostMapping("/admin/addMaquina")
+	public String anadirMaquinas(Maquina maquina, Model model) {
+		maquinaService.crearMaquina(maquina);
+		return "redirect:/user/listarMaquinas";
+	}
+	
+	@PostMapping("/admin/deleteMaquina")
+	public String deleteUsuario(Long id, Model model) {
+		maquinaService.deleteMaquinaById(id);
+		return "redirect:/user/listarMaquinas";
+	}
+	
+	@GetMapping("/user/verMaquina/{id}")
+	public String verMaquina(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("maquina", (Maquina) maquinaService.findMaquinaById(id).orElseThrow());
+		return "mostrarMaquina";
 	}
 	
 }
